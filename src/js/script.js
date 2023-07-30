503a6e4521524d8707d86b1b4158b537e6b30d90
Maryam <mnuriyeva96@gmail.com>
2023-07-30 22:36:05 +0400

    creating menu-sandwich

--- src/js/script.js
+++ src/js/script.js
@@ -54,3 +54,17 @@ updatePrice = function() {
        }
     }
     
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () => {
+   const visibility = primaryNav.getAttribute('data-visible');
+
+   if(visibility === "false"){
+      primaryNav.setAttribute('data-visible', true);
+      navToggle.setAttribute('aria-expanded', true);
+   } else{
+      primaryNav.setAttribute('data-visible', false);
+      navToggle.setAttribute('aria-expanded', false);
+   }
+});
