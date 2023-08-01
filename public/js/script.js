b4a21ef9377a45d611d55d7e0f51f56f713ccb53
Maryam <mnuriyeva96@gmail.com>
2023-08-02 00:26:02 +0400

    deleting menu-sandwich

--- public/js/script.js
+++ public/js/script.js
@@ -56,18 +56,3 @@ var returnSubscriptionPrice = function(bedscount_value)
 window.onload = function () {
    updatePrice();
 };
-
-const primaryNav = document.querySelector('.primary-navigation');
-const navToggle = document.querySelector('.mobile-nav-toggle');
-
-navToggle.addEventListener('click', () => {
-   const visibility = primaryNav.getAttribute('data-visible');
-
-   if(visibility === "false"){
-      primaryNav.setAttribute('data-visible', true);
-      navToggle.setAttribute('aria-expanded', true);
-   } else{
-      primaryNav.setAttribute('data-visible', false);
-      navToggle.setAttribute('aria-expanded', false);
-   }
-});
